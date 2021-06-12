cae88c6a060a4d7545249bf20fdb772f835bd80c
Yasu Watanabe <ywata@protonmail.com>
2021-06-13 07:27:36 +0900

    Update diagram.

---
+++ src/Comparison.hs
@@ -0,0 +1,27 @@
+module Comparison where
+
+
+import Prelude (Bool(..), const)
+
+-- This part compares what we learned so far against popular programming language Java.
+-- Controll structure
+-- if then else
+
+ifte = if True
+       then 1
+       else 2
+
+-- switch
+cas = case True of
+        True -> 1
+        False -> 2
+
+-- loop
+
+
+-- function
+
+
+
+
+
